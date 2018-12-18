14444bade064a38a88db16d290b3188d65725c1d
Allan Simonsen <allansimonsen1972@gmail.com>
2018-12-18 17:27:28 +0100

    Added ComponentHierarchy.py. Updated README.md

---
+++ ComponentHierarchy.py
@@ -0,0 +1,73 @@
+# This script generates an overview of the angular component hierachi
+import os
+import os.path
+import re
+
+class Component:
+    TsFilename = "",
+    TemplateFilename = "",
+    Selector = "",
+    SubComponents = [],
+    IsRoot = True
+
+    def __init__(self, tsFilename: str, templateFilename: str, selector: str, subcomponents: list, isRoot: bool):
+        self.TsFilename = tsFilename
+        self.TemplateFilename = templateFilename
+        self.Selector = selector
+        self.SubComponents = subcomponents
+        self.IsRoot = isRoot
+
+def DisplayComponent(subComponents: list, selector: str, indent: int):
+    indentation = " " * (indent * 3)
+    if (len(subComponents) == 0):
+        print(indentation + "<" + selector + "></" + selector + ">")
+    else:
+        print(indentation + "<" + selector + ">")
+        for subComponent in subComponents:
+            DisplayComponent(subComponent.SubComponents, subComponent.Selector, indent+1)
+        print(indentation + "</" + selector + ">")
+
+# find all files recursively under the current folder that ends with *.component.ts
+components = [os.path.join(dp, f) for dp, dn, filenames in os.walk(".") for f in filenames if f.endswith(".component.ts")]
+compHash = {}
+
+for component in components:
+    componentDefinitionFound = False
+    currentComponent = Component(component, "", "", [], True)
+    f = open(component)
+    line = f.readline()
+    while line:
+        match = re.match(r"@Component\({", line)
+        if match:
+            componentDefinitionFound = True
+        if componentDefinitionFound:
+            match = re.match(r".*templateUrl:.+/(.+)'", line, re.IGNORECASE)
+            if match:
+                currentComponent.TemplateFilename = os.path.join(os.path.dirname(component), match.group(1))
+            match = re.match(r".*selector:.+'(.+)'", line, re.IGNORECASE)
+            if match:
+                currentComponent.Selector = match.group(1)
+            match = re.match(r"}\)", line) # Have we reached the end of the component definition?
+            if match:
+                break # If we did find the end, then stop reading
+        line = f.readline()
+    f.close()
+    compHash[currentComponent.Selector] = currentComponent
+
+for selector1 in compHash:
+    f = open(compHash[selector1].TemplateFilename)
+    template = f.read() # We read the entire template file 
+    for selector2 in compHash: # then we check if the template contains each of the selectors we found in the components
+        pattern = "</" + selector2 + ">"
+        index = template.find(pattern)
+        if index >= 0:
+            compHash[selector1].SubComponents.append(compHash[selector2])
+            compHash[selector2].IsRoot = False # If selector2 has been found in a template then it is not root
+    f.close()
+
+for selector in compHash:
+    if compHash[selector].IsRoot == True:
+        print("Found root: " + selector)
+        DisplayComponent(compHash[selector].SubComponents, selector, 0)
+        print()
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AngularTools
-Tools for when developing Angular
+
+This is supposed to be a collection of tools for helping developing Angular projects.
+
+And a collections always starts with a single tool and then it will grow as we find new handy tools.
+
+* `ComponentHierarchy.py` is a tool that scans the Angular applications components and print out the hierarchy of the components. This is a quick way to get an overview of your project.
