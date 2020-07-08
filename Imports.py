6950e1e3833d50ac617d062256ba0aedc673d52c
Allan Simonsen <allansimonsen1972@gmail.com>
2020-07-08 04:46:44 +0200

    Script now ignores index.ts files when scanning for imports.

--- Imports.py
+++ Imports.py
@@ -1,13 +1,13 @@
 import os
 import os.path
 import re
-
+ 
 # find all files recursively under the current folder that ends with *.ts
 typescriptFiles = [os.path.join(dp, f) for dp, dn, filenames in os.walk(".") for f in filenames if f.endswith(".ts")]
 imports = {}
-
+ 
 for tsFile in typescriptFiles:
-    if "node_modules" not in tsFile:
+    if "node_modules" not in tsFile and "index.ts" not in tsFile:
         f = open(tsFile)
         line = f.readline()
         while line:
@@ -15,14 +15,14 @@ for tsFile in typescriptFiles:
             if match:
                 # print(f"tsFile: {tsFile} - {match.group(1)}")
                 key = match.group(1)
-                if(not key.startswith(".")):
-                    if(key in imports.keys()):
-                        imports[match.group(1)] = imports[match.group(1)] + 1
-                    else:
-                        imports[match.group(1)] = 1
+                # if(not key.startswith(".")):
+                if(key in imports.keys()):
+                    imports[match.group(1)] = imports[match.group(1)] + 1
+                else:
+                    imports[match.group(1)] = 1
             line = f.readline()
         f.close()
-
+ 
 keys = sorted(imports.keys())
 for imps in keys:
     print(f"{imps}: {imports[imps]}")
