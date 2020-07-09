08d817235a6203a38e7296e5c09d700d9db53b17
Allan Simonsen <allansimonsen1972@gmail.com>
2020-07-09 04:29:31 +0200

    Changed to use python teplate for generating the output.

--- ComponentHierarchyDgml.py
+++ ComponentHierarchyDgml.py
@@ -2,7 +2,8 @@
 import os
 import os.path
 import re
-
+import xml.dom.minidom
+ 
 class Component:
     def __init__(self, tsFilename: str, templateFilename: str, selector: str, subcomponents: list, isRoot: bool):
         self.TsFilename = tsFilename
@@ -10,7 +11,7 @@ class Component:
         self.Selector = selector
         self.SubComponents = subcomponents
         self.IsRoot = isRoot
-
+ 
 def GenerateDirectedGraphNodes(components: list, component: Component, isRoot: bool, includeLabel: bool, includeTsFilename: bool, tagName: str, idAttr: str):
     label = ""
     rootComponentCategory = ""
@@ -27,7 +28,7 @@ def GenerateDirectedGraphNodes(components: list, component: Component, isRoot: b
             result = GenerateDirectedGraphNodes(subComponent.SubComponents, subComponent, subComponent.IsRoot, includeLabel, includeTsFilename, tagName, idAttr)
             output = f"{output}{result}"
     return output
-
+ 
 def GenerateDirectedGraphLinks(subComponents: list, displayName: str, parentDisplayName: str, tagName: str, sourceAttr: str, targetAttr: str):
     if len(parentDisplayName) > 0:
         output = f"   <{tagName} {sourceAttr}=\"{parentDisplayName}\" {targetAttr}=\"{displayName}\"/>\n"
@@ -38,11 +39,11 @@ def GenerateDirectedGraphLinks(subComponents: list, displayName: str, parentDisp
             result = GenerateDirectedGraphLinks(subComponent.SubComponents, subComponent.Selector, displayName, tagName, sourceAttr, targetAttr)
             output = f"{output}{result}"
     return output
-
+ 
 # find all files recursively under the current folder that ends with *.component.ts
 components = [os.path.join(dp, f) for dp, dn, filenames in os.walk(".") for f in filenames if f.endswith(".component.ts")]
 compHash = {}
-
+ 
 for component in components:
     if "node_modules" not in component:
         componentDefinitionFound = False
@@ -69,24 +70,25 @@ for component in components:
             line = f.readline()
         f.close()
         compHash[currentComponent.Selector] = currentComponent
-
+ 
 for selector1 in compHash:
-    f = open(compHash[selector1].TemplateFilename)
-    template = f.read() # We read the entire template file 
-    for selector2 in compHash: # then we check if the template contains each of the selectors we found in the components
-        pattern = f"</{selector2}>"
-        index = template.find(pattern)
-        if index >= 0:
-            compHash[selector1].SubComponents.append(compHash[selector2])
-            compHash[selector2].IsRoot = False # If selector2 has been found in a template then it is not root
-        else:
-            pattern = f" {selector2}"
+    if os.path.exists(compHash[selector1].TemplateFilename):
+        f = open(compHash[selector1].TemplateFilename)
+        template = f.read() # We read the entire template file 
+        for selector2 in compHash: # then we check if the template contains each of the selectors we found in the components
+            pattern = f"</{selector2}>"
             index = template.find(pattern)
             if index >= 0:
                 compHash[selector1].SubComponents.append(compHash[selector2])
                 compHash[selector2].IsRoot = False # If selector2 has been found in a template then it is not root
-    f.close()
-
+            else:
+                pattern = f" {selector2}"
+                index = template.find(pattern)
+                if index >= 0:
+                    compHash[selector1].SubComponents.append(compHash[selector2])
+                    compHash[selector2].IsRoot = False # If selector2 has been found in a template then it is not root
+        f.close()
+ 
 nodes = ""
 links = ""
 for selector in compHash:
@@ -96,21 +98,23 @@ for selector in compHash:
         nodes = nodes + GenerateDirectedGraphNodes(component.SubComponents, component, True, False, True, "Node", "Id")
         links = links + GenerateDirectedGraphLinks(component.SubComponents, selector, "", "Link", "Source", "Target")
         print()
-
+ 
+fileContent = f'''\
+<?xml version='1.0' encoding='utf-8'?>
+<DirectedGraph GraphDirection="TopToBottom" Layout="ForceDirected" ZoomLevel="-1" xmlns="http://schemas.microsoft.com/vs/2009/dgml">
+<Nodes>\n{nodes}</Nodes>
+<Links>\n{links}</Links>
+<Categories>
+    <Category Id="RootComponent" Label="Root component" Background="#FF00AA00" IsTag="True" />
+</Categories>
+<Properties>
+    <Property Id="ComponentFilename" DataType="System.String" />
+</Properties>
+</DirectedGraph>
+'''
+ 
 outputFilename = f"ReadMe-ProjectStructure.dgml"
 print(f"Generating directed graph (dgml) to file: {outputFilename}")
 file = open(outputFilename, "w")
-file.write(
-    f"<?xml version='1.0' encoding='utf-8'?>\n"\
-    f"<DirectedGraph GraphDirection=\"TopToBottom\" Layout=\"ForceDirected\" ZoomLevel=\"-1\" xmlns=\"http://schemas.microsoft.com/vs/2009/dgml\">\n"\
-    f"<Nodes>\n{nodes}</Nodes>\n"\
-    f"<Links>\n{links}</Links>\n"\
-    "<Categories>\n"\
-    "  <Category Id=\"RootComponent\" Label=\"Root component\" Background=\"#FF00AA00\" IsTag=\"True\" />\n"\
-    "</Categories>\n"\
-    "<Properties>\n"\
-    "  <Property Id=\"ComponentFilename\" DataType=\"System.String\" />\n"\
-    "</Properties>\n"\
-    "</DirectedGraph>\n"
-)
+file.write(fileContent)
 file.close()
